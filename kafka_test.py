b4d9414b8e08654035d62df0ec3f4a27495d3e91
Sambda <45852717+Sambda@users.noreply.github.com>
2021-04-12 20:33:02 +0200

    Add files via upload
    test kafka script

---
+++ kafka_test.py
@@ -0,0 +1,25 @@
+from tweepy.streaming import StreamListener
+from tweepy import OAuthHandler
+from tweepy import Stream
+from kafka import KafkaProducer
+import json
+
+access_token = ""
+access_token_secret =  ""
+api_key =  ""
+api_secret =  ""
+
+class StdOutListener(StreamListener):
+    def on_data(self, data):
+        json_ = json.loads(data) 
+        producer.send("basic", json_["text"].encode('utf-8'))
+        return True
+    def on_error(self, status):
+        print (status)
+
+producer = KafkaProducer(bootstrap_servers='localhost:9092')
+l = StdOutListener()
+auth = OAuthHandler(api_key, api_secret)
+auth.set_access_token(access_token, access_token_secret)
+stream = Stream(auth, l)
+stream.filter(track=["covid19", "corona virus"])
