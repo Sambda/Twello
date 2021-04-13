218781048000f49fa65b891666f4b57c5404e227
Sambda <45852717+Sambda@users.noreply.github.com>
2021-04-13 02:42:03 +0200

    Add files via upload
    Example for an Kafka Topic Consumer, Don't forget to change to kafka topic name.

---
+++ Consumer_kafka_topic.py
@@ -0,0 +1,25 @@
+import json
+from time import sleep
+
+from kafka import KafkaConsumer
+
+if __name__ == '__main__':
+    #Change Topic name 
+    parsed_topic_name = 'twitter_stream_covid'
+    followers_count_threshold = 200
+
+    consumer = KafkaConsumer(parsed_topic_name, auto_offset_reset='earliest',
+                             bootstrap_servers=['localhost:9092'], api_version=(0, 10), consumer_timeout_ms=1000)
+    for msg in consumer:
+        record = json.loads(msg.value)
+        id_user = int(record['id'])
+        name = record['user']['name']
+        followers_count = int(record['user']['followers_count'])
+        text = record['text']
+
+        if followers_count > followers_count_threshold:
+            print('followers_count: {}, name: {}'.format(followers_count, name))
+        sleep(3)
+
+    if consumer is not None:
+        consumer.close()
