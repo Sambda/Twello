fc0a18ac9bf96504c90c35c812a572db6b07bfbc
Sambda <45852717+Sambda@users.noreply.github.com>
2021-04-13 14:23:02 +0200

    Add files via upload
    updated Consumer and Producer Kafka.
    Just enter a hashtag in console, by running producer.
     The create topic will have the name "topic_" + hashtag → z.B hasttag = dog → topic_dog
    Enter the topicname in consule by running consumer.

--- Consumer_kafka_topic.py
+++ Consumer_kafka_topic.py
@@ -3,15 +3,19 @@ from time import sleep
 
 from kafka import KafkaConsumer
 
+# Get topic from console input
+parsed_topic_name = input("Enter Topic name : ")
+
 if __name__ == '__main__':
-    #Change Topic name 
-    parsed_topic_name = 'twitter_stream_covid'
+    #Change Topic name, If not over console input
+    #parsed_topic_name = 'twitter_stream_covid'
     followers_count_threshold = 200
 
     consumer = KafkaConsumer(parsed_topic_name, auto_offset_reset='earliest',
                              bootstrap_servers=['localhost:9092'], api_version=(0, 10), consumer_timeout_ms=1000)
     for msg in consumer:
         record = json.loads(msg.value)
+        # Some examples for reading keys from twitter
         id_user = int(record['id'])
         name = record['user']['name']
         followers_count = int(record['user']['followers_count'])
@@ -19,7 +23,8 @@ if __name__ == '__main__':
 
         if followers_count > followers_count_threshold:
             print('followers_count: {}, name: {}'.format(followers_count, name))
-        sleep(3)
+        # delete sleep if not just testing
+        sleep(1)
 
     if consumer is not None:
         consumer.close()
---
+++ StreamProducer.py
@@ -0,0 +1,52 @@
+from kafka import KafkaProducer
+import kafka
+import json
+import tweepy
+from tweepy import OAuthHandler
+from tweepy import Stream
+from tweepy.streaming import StreamListener
+
+# TWITTER API CONFIGURATIONS
+access_token = "1380977247067209728-zjUF158nYIUPQVzMgTLwBJvHUpOwTW"
+access_token_secret =  "toLJYQRur9fU3va7eNrLrSw6xY3t9JZV7YLAhzby4gGCn"
+api_key =  "2uKUCZgWqrLZFr0nGC4kw4ven"
+api_secret =  "Gk17pGWblSkAB4N57abSCY70LB3IXUcenNEJWPadKhMkIGkP2w"
+
+hashtag = input("Enter the hashtag : ")
+print("Created Topic name: topic_{}".format(hashtag))
+# TWITTER API AUTH
+auth = OAuthHandler(api_key, api_secret)
+auth.set_access_token(access_token, access_token_secret)
+
+api = tweepy.API(auth)
+
+
+# Twitter Stream Listener
+class KafkaPushListener(StreamListener):
+    def __init__(self):
+        # localhost:9092 = Default Zookeeper Producer Host and Port Adresses
+        self.producer = KafkaProducer(bootstrap_servers=['localhost:9092'])
+
+    # Get Producer that has topic name is Twitter
+    # self.producer = self.client.topics[bytes("twitter")].get_producer()
+
+    def on_data(self, data):
+        # Producer produces data for consumer
+        # Data comes from Twitter
+        self.producer.send("topic_" + hashtag, data.encode('utf-8'))
+        #self.producer.send("twitter_stream_" + hashtag, data.encode('utf-8'))
+        print(data)
+        return True
+
+    def on_error(self, status):
+        print(status)
+        return True
+
+
+# Twitter Stream Config
+twitter_stream = Stream(auth, KafkaPushListener())
+
+hashStr = "#"+ hashtag
+
+# Produce Data that has trump hashtag (Tweets)
+twitter_stream.filter(track=[hashStr])
