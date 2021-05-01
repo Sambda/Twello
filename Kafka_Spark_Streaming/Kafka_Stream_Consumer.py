fdc0046f83f28b75797517078c325eb83b2f734a
Cilia Hegestweiler <hegstci@Cilias-MBP.fritz.box>
2021-05-01 18:01:16 +0200

    Refactored Producer and Consumer

--- Kafka_Spark_Streaming/Kafka_Stream_Consumer.py
+++ Kafka_Spark_Streaming/Kafka_Stream_Consumer.py
@@ -1,48 +1,81 @@
 from kafka import KafkaConsumer
 from prometheus_client import start_http_server, Counter, Gauge
 import json
+import threading
+import random as rdm
 from time import sleep
 
-#consum_counter = Counter("consume_counter", "Counts all received Tweets of this producer")
-#compression_rate_avg_gauge = Gauge("compression_rate_avg", "compression")
+consumer_counter = Counter("consume_counter", "Counts all received Tweets of this consumer-group", ['topic'])
+records_lag_max_gauge = Gauge("records_lag_max", "records_lag_max", ['topic'])
+bytes_consumed_rate_gauge = Gauge("bytes_consumed_rate", "bytes_consumed_rate", ['topic'])
+fetch_rate_gauge = Gauge("fetch_rate", "fetch_rate", ['topic'])
 
-records_lag_max_gauge = Gauge("records_lag_max", "records_lag_max")
-bytes_consumed_rate_gauge = Gauge("bytes_consumed_rate", "bytes_consumed_rate")
-fetch_rate_gauge = Gauge("fetch_rate", "fetch_rate")
 
-# Get topic from console input
-#parsed_topic_name = input("Enter Topic name : ")
-parsed_topic_name = "topic_bigdata"
+def create_consumer_group(topic_name, number_of_consumers):
+    consumer_group = []
+    for i in range(0, number_of_consumers):
+        consumer_group.append(consumer_thread(i, topic_name))
+    return consumer_group
+
+
+def create_consumer(topic_name):
+    parsed_topic_name = 'topic_' + topic_name
+    return KafkaConsumer(parsed_topic_name, auto_offset_reset='earliest',
+                         bootstrap_servers=['localhost:9092'], api_version=(0, 10), consumer_timeout_ms=1000)
+
+
+class consumer_thread(threading.Thread):
+    def __init__(self, threadID, topic_name):
+        threading.Thread.__init__(self)
+        self.threadID = threadID
+        self.topic = topic_name
+        self.name = topic_name + "consumer" + str(threadID)
+        self.consumer = create_consumer(topic_name)
+
+    def run(self):
+        print("Starting " + self.name)
+        consume(self.consumer, self.topic, self.threadID)
+        print("Exiting " + self.name)
+
+
+def consume(cons, topic, id):
+    for msg in cons:
+        print(str(id) + "consumed")
+        record = json.loads(msg.value)
+        handle_metrics(cons.metrics(), topic)
+        sleep(rdm.random())
+
+    if cons is not None:
+        cons.close()
+
+
+def handle_metrics(metrics, topic):
+    records_lag_max_gauge.labels(topic).set(metrics["consumer-fetch-manager-metrics"]["records-lag-max"])
+    bytes_consumed_rate_gauge.labels(topic).set(metrics["consumer-fetch-manager-metrics"]["bytes-consumed-rate"])
+    fetch_rate_gauge.labels(topic).set(metrics["consumer-fetch-manager-metrics"]["fetch-rate"])
+    consumer_counter.labels(topic).inc()
+
 
 if __name__ == '__main__':
-    start_http_server(8000)
-    #Change Topic name, If not over console input
-    #followers_count_threshold = 200
 
-    consumer = KafkaConsumer(parsed_topic_name, auto_offset_reset='earliest',
-                             bootstrap_servers=['localhost:9092'], api_version=(0, 10), consumer_timeout_ms=1000)
+    # Get topic from console input
+    input_topics = input("Enter Topic names (seperated by ','): ")
+    topic_names = input_topics.replace(" ", "").split(",")
 
-    #print(consumer.metrics())
+    consumer_per_topic = int(input("Enter a number of consumers per topic (Defines the consumer group size: "))
 
-    for msg in consumer:
-        record = json.loads(msg.value)
-        records_lag_max_gauge.set(consumer.metrics()["consumer-fetch-manager-metrics"]["records-lag-max"])
-        bytes_consumed_rate_gauge.set(consumer.metrics()["consumer-fetch-manager-metrics"]["bytes-consumed-rate"])
-        fetch_rate_gauge.set(consumer.metrics()["consumer-fetch-manager-metrics"]["fetch-rate"])
-        sleep(1)
-        '''
-        # Some examples for reading keys from twitter
-        id_user = int(record['id'])
-        name = record['user']['name']
-        followers_count = int(record['user']['followers_count'])
-        text = record['text']
+    start_http_server(8001)
 
-        if followers_count > followers_count_threshold:
-            print('followers_count: {}, name: {}'.format(followers_count, name))
-        # delete sleep if not just testing
-        consum_counter.inc()
-        sleep(1)
-        '''
+    consumer_groups = {topic_name: create_consumer_group(topic_name, consumer_per_topic) for topic_name in topic_names}
 
-    if consumer is not None:
-        consumer.close()
+    # Start Consumer Threads
+    for topic in topic_names:
+        counter = 0
+        for consumer_thread in consumer_groups[topic]:
+            consumer_thread.start()
+
+    # Wait for al Consumer Threads to finish
+    for topic in topic_names:
+        counter = 0
+        for consumer_thread in consumer_groups[topic]:
+            consumer_thread.join()
--- Kafka_Spark_Streaming/Kafka_Stream_Producer.py
+++ Kafka_Spark_Streaming/Kafka_Stream_Producer.py
@@ -1,30 +1,19 @@
 from kafka import KafkaProducer
-import json
 import tweepy
 from tweepy import OAuthHandler
 from tweepy import Stream
 from tweepy.streaming import StreamListener
 from prometheus_client import start_http_server, Counter, Gauge
 
-# Console Input for the wanted hashtag
-#hashtag = input("Enter the hashtag : ")
-hashtag = "bigdata"
-print("Created Topic name: topic_{}".format(hashtag))
 
 # TWITTER API CONFIGURATIONS
 access_token = "1380977247067209728-zjUF158nYIUPQVzMgTLwBJvHUpOwTW"
-access_token_secret =  "toLJYQRur9fU3va7eNrLrSw6xY3t9JZV7YLAhzby4gGCn"
-api_key =  "2uKUCZgWqrLZFr0nGC4kw4ven"
-api_secret =  "Gk17pGWblSkAB4N57abSCY70LB3IXUcenNEJWPadKhMkIGkP2w"
+access_token_secret = "toLJYQRur9fU3va7eNrLrSw6xY3t9JZV7YLAhzby4gGCn"
+api_key = "2uKUCZgWqrLZFr0nGC4kw4ven"
+api_secret = "Gk17pGWblSkAB4N57abSCY70LB3IXUcenNEJWPadKhMkIGkP2w"
 
-# TWITTER API AUTH
-auth = OAuthHandler(api_key, api_secret)
-auth.set_access_token(access_token, access_token_secret)
-api = tweepy.API(auth)
-
-#Prometheus
-#tweet_counter = Counter("received_tweets", "Counts all received Tweets of this producer")
-#user_counter = Counter("user", "User all received Tweets of this producer")
+# Prometheus Metrics
+tweet_counter = Counter("received_tweets", "Counts all received Tweets of this producer", ['topic'])
 compression_rate_avg_gauge = Gauge("compression_rate_avg", "compression")
 request_latency_avg_gauge = Gauge("request_latency_avg", "latency")
 batch_size_avg_gauge = Gauge("batch_size_avg", "batch_size_avg")
@@ -34,30 +23,27 @@ batch_size_max_gauge = Gauge("batch_size_max", "batch_size_max")
 # Twitter Stream Listener
 class KafkaPushListener(StreamListener):
     def __init__(self):
-        # localhost:9092 = Default Zookeeper Producer Host and Port Adresses
+        # localhost:9092 = Default Kafka Advertisment Host and Port Adress
         self.producer = KafkaProducer(bootstrap_servers=['localhost:9092'])
-        start_http_server(8000)
-    # Get Producer that has topic name is Twitter
-    # self.producer = self.client.topics[bytes("twitter")].get_producer()
 
     def on_data(self, data):
         # Producer produces data for consumer
         # Data comes from Twitter
-        self.producer.send("topic_" + hashtag, data.encode('utf-8'))
 
-        #Pretty print tweet on console
-        #parsed = json.loads(data)
-        #print(json.dumps(parsed, indent=4, sort_keys=True))
+        # Parse Data into JSON
+        # data_json = json.loads(data.encode('utf-8'))
 
-        #Metric Stuff
-        #tweet_counter.inc()
+        # Pretty print tweet on console
+        # print(json.dumps(data_json['text'], indent=4, sort_keys=True))
+        # print(json.dumps(data_json['entities']['hashtags'], indent=4, sort_keys=True))
 
-        print(self.producer.metrics())
-        compression_rate_avg_gauge.set(self.producer.metrics()["producer-metrics"]["compression-rate-avg"])
-        request_latency_avg_gauge.set(self.producer.metrics()["producer-metrics"]["request-latency-avg"])
-        batch_size_avg_gauge.set(self.producer.metrics()["producer-metrics"]["batch-size-avg"])
-        batch_size_max_gauge.set(self.producer.metrics()["producer-metrics"]["batch-size-max"])
+        topics = determinate_topic(data)
 
+        # Send data to topic
+        for topic in topics:
+            self.producer.send("topic_" + topic, data.encode('utf-8'))
+
+        handle_metrics(self.producer.metrics(), topics)
         return True
 
     def on_error(self, status):
@@ -65,11 +51,41 @@ class KafkaPushListener(StreamListener):
         return True
 
 
-# Twitter Stream Config
-twitter_stream = Stream(auth, KafkaPushListener())
+def determinate_topic(data):
+    topics = []
+    for tag in hashtags:
+        if tag in data.lower():
+            topics.append(tag.replace("#", ""))
+    return topics
 
-hashStr = "#"+ hashtag
 
-# Produce Data that has trump hashtag (Tweets)
-#twitter_stream.filter(locations=[-180, -90, 180, 90])
-twitter_stream.filter(track=[hashStr])
+def handle_metrics(metrics, topics):
+    for topic in topics:
+        tweet_counter.labels(topic).inc()
+    compression_rate_avg_gauge.set(metrics["producer-metrics"]["compression-rate-avg"])
+    request_latency_avg_gauge.set(metrics["producer-metrics"]["request-latency-avg"])
+    batch_size_avg_gauge.set(metrics["producer-metrics"]["batch-size-avg"])
+    batch_size_max_gauge.set(metrics["producer-metrics"]["batch-size-max"])
+
+
+if __name__ == '__main__':
+
+    # Startup Metrics Endpoint
+    start_http_server(8000)
+
+    # Get topic from console input
+    producer_keywords = input(
+        "Enter Keywords to Track in Twitter API - those Keywords are the created topics (seperated by ','): ")
+    keywords = producer_keywords.lower().replace(" ", "").split(",")
+    hashtags = ['#{}'.format(keyword) for keyword in keywords]
+
+    # TWITTER API AUTH
+    auth = OAuthHandler(api_key, api_secret)
+    auth.set_access_token(access_token, access_token_secret)
+    api = tweepy.API(auth)
+
+    # Twitter Stream Config
+    twitter_stream = Stream(auth, KafkaPushListener())
+
+    # Filter the Twitter stream
+    twitter_stream.filter(track=hashtags)
